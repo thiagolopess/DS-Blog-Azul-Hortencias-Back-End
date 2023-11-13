84249119476f4d80c6e69e00e280f308e0f5cccb
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-11-13 13:17:54 -0300

    Inserts create and drop sql scripts

---
+++ src/main/resources/database_scripts/drop.sql
@@ -0,0 +1,15 @@
+-- Drop Sequences
+drop sequence sq_account;
+drop sequence sq_comment;
+drop sequence sq_post;
+drop sequence sq_forum;
+
+-- Drop Tables
+drop table TB_COMMENT;
+drop table TB_POST;
+drop table TB_FORUM;
+drop table TB_ACCOUNT;
+
+-- Drop Data types
+drop type tp_role;
+drop type tp_forum;
