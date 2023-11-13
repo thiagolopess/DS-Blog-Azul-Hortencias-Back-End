59dc12aa94e13cb16ae3417c811dd2b53f301118
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-11-13 12:05:26 -0300

    Adds Spring's base project and its dependencies

---
+++ src/main/java/unb/azul/hortencias/blog/feudal/BlogFeudalApplication.java
@@ -0,0 +1,13 @@
+package unb.azul.hortencias.blog.feudal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFeudalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogFeudalApplication.class, args);
+	}
+
+}
